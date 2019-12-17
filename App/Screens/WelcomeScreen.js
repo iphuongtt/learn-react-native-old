31ec32a12470a678bd81569ea330d5da8cc904bd
Trinh Thanh Phuong <iphuongtt@gmail.com>
2019-12-17 11:55:23 +0700

    navigation: Basic: push, back

--- App/Screens/WelcomeScreen.js
+++ App/Screens/WelcomeScreen.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
-const WelcomeScreen = () => {
+const WelcomeScreen = (props) => {
   return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
     <Text style={{fontSize: 30}}>This is welcome screen </Text>
+    <Button
+      title="Go to welcome again"
+      onPress={() => props.navigation.push('Welcome')}
+    />
+    <Button
+      title="Go to Home"
+      onPress={() => props.navigation.navigate('Home')}
+    />
+    <Button
+      title="Go Back"
+      onPress={() => props.navigation.goBack()}
+    />
   </View>
 }
 
